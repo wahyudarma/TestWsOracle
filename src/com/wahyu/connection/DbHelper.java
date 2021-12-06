e538d446f38ce5ee4abf8136abe9e2e112205598
wahyudarma <wahyudarma2408@gmail.com>
2021-12-06 14:19:47 +0700

    DbHelper and MainPublisher edited

--- src/com/wahyu/connection/DbHelper.java
+++ src/com/wahyu/connection/DbHelper.java
@@ -1,6 +1,8 @@
 package com.wahyu.connection;
 
+import jdk.nashorn.internal.codegen.CompilerConstants;
 import oracle.jdbc.OracleDriver;
+import oracle.jdbc.OracleTypes;
 
 import java.sql.*;
 
@@ -62,5 +64,85 @@ public class DbHelper
             e.printStackTrace();
         }
         return rs;
+    }
+    public static ResultSet selectPackage(String query)
+    {
+        bukaKoneksi();
+
+        ResultSet rs = null;
+        try {
+            CallableStatement cs = koneksi.prepareCall(query);
+            cs.registerOutParameter(1, OracleTypes.CURSOR);
+            cs.execute();
+            rs = (ResultSet) cs.getObject(1);
+        }
+        catch (SQLException t)
+        {
+            System.err.format("SQL State: %s\n%s", t.getSQLState(), t.getMessage());
+            t.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    public static ResultSet selectPackageById(String query)
+    {
+        bukaKoneksi();
+
+        ResultSet rs = null;
+        try {
+            CallableStatement cs = koneksi.prepareCall(query);
+//            cs.setInt(1, 1);
+            cs.registerOutParameter(1, OracleTypes.INTEGER);
+            cs.registerOutParameter(2, OracleTypes.CURSOR);
+            cs.execute();
+            rs = (ResultSet) cs.getObject(2);
+        }
+        catch (SQLException t)
+        {
+            System.err.format("SQL State: %s\n%s", t.getSQLState(), t.getMessage());
+            t.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    public static int insertPackageGetId(String query){
+        bukaKoneksi();
+        int num = 0;
+        int result = -1;
+        try {
+            CallableStatement cs = koneksi.prepareCall(query);
+            num = cs.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = cs.getGeneratedKeys();
+            if (rs.next()) {
+                result = rs.getInt(1);
+            }
+            rs.close();
+            cs.close();
+        } catch(Exception e){
+            e.printStackTrace();
+            result = -1;
+        }
+        return result;
+    }
+    public static boolean executePackage(String query){
+        bukaKoneksi();
+        boolean result = false;
+        try {
+            CallableStatement cs = koneksi.prepareCall(query);
+            cs.executeUpdate();
+
+            result = true;
+
+            cs.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/com/wahyu/main/MainPublisher.java
+++ src/com/wahyu/main/MainPublisher.java
@@ -2,12 +2,17 @@ package com.wahyu.main;
 
 import javax.xml.ws.Endpoint;
 import com.wahyu.services.EmployeeServices;
+import com.wahyu.services.PackageServices;
 
 public class MainPublisher
 {
     public static void main(String[]args)
     {
-        Endpoint.publish("http://localhost:8889/Java-Ws-Oracle/ws/javaoracle", new EmployeeServices());
+    //  Employee
+//        Endpoint.publish("http://localhost:8889/Java-Ws-Oracle/ws/javaoracle", new EmployeeServices());
+
+    // Package
+        Endpoint.publish("http://localhost:8889/Java-Ws-Oracle/ws/javaoracle", new PackageServices());
     }
 }
 
